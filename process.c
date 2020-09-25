835dd738d5d5643a91955e7ac7a63fabb8bf7704
Cassey Anene <casseyanene@gmail.com>
2020-09-24 22:31:52 -0500

    loop
    removed the for loop in the main file

--- process.c
+++ process.c
@@ -8,35 +8,32 @@ int main(void)
     int pid;
     int child;
 
-    for (int x; x < 2; x++)
+    pid = fork();
+    if (pid == 0)
     {
-        pid = fork();
-        if (pid == 0)
+        //child sleeps for random # of time
+        for (int i = 0; i < (rand() % 30 + 1); i++)
         {
-            //child sleeps for random # of time
-            for (int i = 0; i < (rand() % 30 + 1); i++)
-            {
 
-                // child sleeps for random amount of times
-                printf("Child Pid = %d is going to sleep\n", getpid());
-                // int sleepSecs;
-                sleep(rand() % 10 + 1);
-            }
+            // child sleeps for random amount of times
+            printf("Child Pid = %d is going to sleep\n", getpid());
+            // int sleepSecs;
+            sleep(rand() % 10 + 1);
+        }
 
-            printf("Child Pid = %d is up! Where is my parent = %d?\n", getpid(), getppid());
-            exit(0);
-            break;
-        }
-        if (pid == -1)
-        {
-            break;
-        }
-        else
-        {
-            wait(&status);
-            printf("Child = %d has finished\n", getpid() + 1);
-            break;
-        }
+        printf("Child Pid = %d is up! Where is my parent = %d?\n", getpid(), getppid());
+        exit(0);
+        // break;
+    }
+    if (pid == -1)
+    {
+        // break;
+    }
+    else
+    {
+        wait(&status);
+        printf("Child = %d has finished\n", getpid() + 1);
+        // break;
     }
     printf("Program terminating...\n");
 
